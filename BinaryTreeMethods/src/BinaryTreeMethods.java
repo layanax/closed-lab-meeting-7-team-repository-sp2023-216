f3f9c62ff5aaa4285dd700f336c7376e247c331c
hodous.4 <hodous.4@CSE-DNC238600D.coeit.osu.edu>
2024-02-06 10:04:09 -0500

    we almost did it

--- BinaryTreeMethods/src/BinaryTreeMethods.java
+++ BinaryTreeMethods/src/BinaryTreeMethods.java
@@ -32,10 +32,23 @@ public final class BinaryTreeMethods {
     public static <T> int height(BinaryTree<T> t) {
         assert t != null : "Violation of: t is not null";
 
-        // TODO - fill in body
+        BinaryTree<T> empty = t.newInstance();
+        int toReturn = 0;
+        if (t.size() > 0) {
+            BinaryTree<T> left = t.newInstance();
+            BinaryTree<T> right = t.newInstance();
+            T root = t.disassemble(left, right);
+            int sizeLeft = height(left);
+            int sizeRight = height(right);
+            if (sizeLeft > sizeRight) {
+                toReturn = 1 + height(left);
+            } else {
+                toReturn = 1 + height(right);
+            }
+            t.assemble(root, left, right);
+        }
+        return toReturn;
 
-        // This line added just to make the component compilable.
-        return 0;
     }
 
     /**
@@ -56,10 +69,29 @@ public final class BinaryTreeMethods {
         assert t != null : "Violation of: t is not null";
         assert x != null : "Violation of: x is not null";
 
-        // TODO - fill in body
+        BinaryTree<T> empty = t.newInstance();
+        boolean toReturn = false;
+        if (t.size() > 0) {
+            if (t.root().equals(x)) {
+                toReturn = true;
+            } else {
+                BinaryTree<T> left = t.newInstance();
+                BinaryTree<T> right = t.newInstance();
+                T root = t.disassemble(left, right);
+                if ((left.size() > 0 && left.root().equals(x))
+                        || (right.size() > 0 && right.root().equals(x))) {
+                    toReturn = true;
+                } else if (right.size() > 0) {
+                    toReturn = isInTree(right, x);
+                    if (!toReturn && left.size() > 0) {
+                        toReturn = isInTree(left, x);
+                    }
+                }
+                t.assemble(root, left, right);
+            }
+        }
+        return toReturn;
 
-        // This line added just to make the component compilable.
-        return false;
     }
 
     /**
