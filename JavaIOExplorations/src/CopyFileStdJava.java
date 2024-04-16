0ee550939ef6472759836d2ea4ca6c24343e996f
layan <layan@172.31.2.38>
2024-04-16 09:24:29 -0400

    my version

--- JavaIOExplorations/src/CopyFileStdJava.java
+++ JavaIOExplorations/src/CopyFileStdJava.java
@@ -1,7 +1,14 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Program to copy a text file into another file.
  *
- * @author Put your name here
+ * @author Layan Abdallah & Oak Hodous
  *
  */
 public final class CopyFileStdJava {
@@ -20,7 +27,32 @@ public final class CopyFileStdJava {
      */
     public static void main(String[] args) {
 
-        // TODO - fill in body
+        BufferedReader in;
+        PrintWriter out;
+        try {
+            in = new BufferedReader(new FileReader(args[0]));
+            out = new PrintWriter(new BufferedWriter(new FileWriter(args[1])));
+        } catch (IOException error) {
+            System.err.println("Error: could not open file");
+            return;
+        }
+        try {
+            String input = in.readLine();
+            while (input != null) {
+                out.println(in);
+                input = in.readLine();
+            }
+        } catch (IOException error) {
+            System.err.println(
+                    "Error: could not read from file or write to file");
+        }
+        try {
+
+            in.close();
+            out.close();
+        } catch (IOException error) {
+            System.err.println("Error: could not close file");
+        }
 
     }
 
